5a5ffc55279cb7d96f4c25696f0248f4dac81e84
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-05-15 23:12:26 +0100

    Debloat

--- js/active.js
+++ js/active.js
@@ -1,39 +1,11 @@
 (function ($) {
     'use strict';
 
-    // :: Slick
-    if ($.fn.slick) {
-        $('.slider-for').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            speed: 500,
-            arrows: false,
-            fade: true,
-            asNavFor: '.slider-nav'
-        });
-        $('.slider-nav').slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            speed: 500,
-            asNavFor: '.slider-for',
-            dots: true,
-            centerMode: true,
-            focusOnSelect: true,
-            slide: 'div',
-            autoplay: true,
-            centerMode: true,
-            centerPadding: '30px',
-            mobileFirst: true,
-            prevArrow: '<i class="fa fa-angle-left"></i>',
-            nextArrow: '<i class="fa fa-angle-right"></i>'
-        });
-    }
-
     // :: ScrollUp
     if ($.fn.scrollUp) {
         $.scrollUp({
             scrollSpeed: 500,
-            scrollText: '<i class="fa fa-angle-up"></i>'
+            scrollText: '<i class="ti-arrow-up"></i>'
         });
     }
 
