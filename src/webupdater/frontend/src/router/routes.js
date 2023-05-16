e4d03922e7a27d9404218598eccbe06af1db8082
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-05-16 02:57:08 +0100

    Fix routes again

--- src/webupdater/frontend/src/router/routes.js
+++ src/webupdater/frontend/src/router/routes.js
@@ -4,7 +4,7 @@ const routes = [
     path: '/webupdaterframe',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '/', component: () => import('pages/Device.vue') }
+      { path: '/webupdaterframe', component: () => import('pages/Device.vue') }
     ]
   }
 ]
